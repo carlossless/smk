682961f5dd3d8cfd7fa2bff4be8f8a67e4b72dff
Karolis Stasaitis <contact@carlossless.io>
2023-12-31 12:20:43 +0100

    clang-format usage for headers

--- src/uart.h
+++ src/uart.h
@@ -1,9 +1,9 @@
 #ifndef UART_H
 #define UART_H
 
-void uart_init();
-void uart_putc(unsigned char c);
+void          uart_init();
+void          uart_putc(unsigned char c);
 unsigned char uart_getc();
-void uart_interrupt_handler() __interrupt (_INT_EUART0);
+void          uart_interrupt_handler() __interrupt(_INT_EUART0);
 
 #endif
