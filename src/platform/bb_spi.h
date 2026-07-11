388ebff401c3d10a812099f802e71086f69c5584
Karolis Stasaitis <contact@carlossless.io>
2026-07-11 13:43:35 +0200

    rf: rework wireless link handling and fix stuck pairing

--- src/platform/bb_spi.h
+++ src/platform/bb_spi.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 
-void bb_spi_xfer(uint8_t *data, int len);
+bool bb_spi_xfer(uint8_t *data, int len);
+
+void bb_spi_recv(uint8_t *data, int len);
--- src/smk/keyboard.c
+++ src/smk/keyboard.c
@@ -7,8 +7,12 @@ __xdata keymap_config_t keymap_config;
 
 void keyboard_init()
 {
-    keyboard_state.led_state = 0x00;
-    keyboard_state.rf_link   = 0x00;
+    keyboard_state.led_state     = 0x00;
+    keyboard_state.rf_link       = 0x00;
+    keyboard_state.battery_level = 0x00;
+    keyboard_state.low_power     = 0x00;
+    keyboard_state.connected     = 0x00;
+    keyboard_state.paired        = 0x00;
 
 #ifdef NKRO_ENABLE
     keymap_config.nkro = 1;
