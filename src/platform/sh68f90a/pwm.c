d1ba518e5a2f94658dea32f9625515ab3fbe560d
Karolis Stasaitis <contact@carlossless.io>
2026-07-11 13:43:35 +0200

    led: shared LED animation effect engine

--- src/platform/sh68f90a/pwm.c
+++ src/platform/sh68f90a/pwm.c
@@ -1,9 +1,4 @@
 #include "pwm.h"
 #include <stdint.h>
-#include <stdio.h>
-#include "matrix.h"
 
-void pwm_interrupt_handler() __interrupt(_INT_PWM0)
-{
-    matrix_scan_step();
-}
+void pwm_interrupt_handler() __interrupt(_INT_PWM0) {}
