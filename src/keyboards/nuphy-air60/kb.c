f2cad821e25befa8f4190247bbc299f344f3f671
Karolis Stasaitis <contact@carlossless.io>
2026-07-11 13:51:44 +0200

    nuphy: OS-slider base-layer selection and keymap fixes

--- src/keyboards/nuphy-air60/kb.c
+++ src/keyboards/nuphy-air60/kb.c
@@ -3,7 +3,9 @@
 #include "keycodes.h"
 #include "kbdef.h"
 #include "keyboard.h"
-#include "debug.h"
+#include "layout.h"
+#include "settings.h"
+#include "debug.h" // dprintf
 #include "report.h"
 #include "usb.h"
 
@@ -32,30 +34,63 @@ void kb_init()
 {
     user_keyboard_state.conn_mode = CONN_MODE_SWITCH;
     user_keyboard_state.os_mode   = OS_MODE_SWITCH;
+
+    set_default_layer(layout_os_base_layer(user_keyboard_state.os_mode == KEYBOARD_OS_MODE_MAC));
+
+#ifdef RF_ENABLED
+    rf_set_mac_mode_compat(user_keyboard_state.os_mode == KEYBOARD_OS_MODE_MAC);
+#endif
 }
+
+#define SLIDER_DEBOUNCE_ITERS 256
 
 void kb_update_switches()
 {
-    if (user_keyboard_state.conn_mode != CONN_MODE_SWITCH) {
-        user_keyboard_state.conn_mode = CONN_MODE_SWITCH;
+    static __xdata uint16_t conn_debounce;
+    static __xdata uint16_t os_debounce;
+
+    const uint8_t raw_conn = CONN_MODE_SWITCH;
+    if (raw_conn == user_keyboard_state.conn_mode) {
+        conn_debounce = 0;
+    } else if (++conn_debounce >= SLIDER_DEBOUNCE_ITERS) {
+        conn_debounce                 = 0;
+        user_keyboard_state.conn_mode = raw_conn;
         switch (user_keyboard_state.conn_mode) {
             case KEYBOARD_CONN_MODE_USB:
                 dprintf("USB_MODE\r\n");
+#ifdef RF_ENABLED
+                rf_apply_usb_mode();
+#endif
                 break;
             case KEYBOARD_CONN_MODE_RF:
                 dprintf("RF_MODE\r\n");
+#ifdef RF_ENABLED
+                rf_set_link((rf_mode_t)user_settings.rf_link);
+                rf_kbd_lazy_state_init();
+#endif
                 break;
         }
     }
 
-    if (user_keyboard_state.os_mode != OS_MODE_SWITCH) {
-        user_keyboard_state.os_mode = OS_MODE_SWITCH;
+    const uint8_t raw_os = OS_MODE_SWITCH;
+    if (raw_os == user_keyboard_state.os_mode) {
+        os_debounce = 0;
+    } else if (++os_debounce >= SLIDER_DEBOUNCE_ITERS) {
+        os_debounce                 = 0;
+        user_keyboard_state.os_mode = raw_os;
+        set_default_layer(layout_os_base_layer(user_keyboard_state.os_mode == KEYBOARD_OS_MODE_MAC));
         switch (user_keyboard_state.os_mode) {
             case KEYBOARD_OS_MODE_MAC:
                 dprintf("MAC_MODE\r\n");
+#ifdef RF_ENABLED
+                rf_set_mac_mode_compat(true);
+#endif
                 break;
             case KEYBOARD_OS_MODE_WIN:
                 dprintf("WIN_MODE\r\n");
+#ifdef RF_ENABLED
+                rf_set_mac_mode_compat(false);
+#endif
                 break;
         }
     }
@@ -91,6 +126,9 @@ extern void indicators_ul_brightness_down();
 extern void indicators_ul_speed_up();
 extern void indicators_ul_speed_down();
 extern void indicators_factory_reset();
+extern void indicators_battery_flash();
+extern void indicators_battery_on();
+extern void indicators_battery_off();
 
 // While UL_MODE (the "?" key on the Fn layer) is held, the RGB_* chords adjust the
 // underglow instead of the main backlight. Held in xdata to spare internal RAM.
@@ -98,6 +136,13 @@ static __xdata bool ul_mode_active;
 
 // While RESET_HOLD (Fn+Tab) is held, pressing FACT_RESET (V) factory-resets settings.
 static __xdata bool reset_mode_active;
+
+#ifdef RF_ENABLED
+#    define LINK_PAIRING_HOLD_TICKS 60000
+static __xdata uint16_t link_hold_ticks    = 0;
+static __xdata uint16_t link_hold_keycode  = 0;
+static __xdata bool     link_pairing_armed = false;
+#endif
 
 bool kb_process_record(uint16_t keycode, bool key_pressed)
 {
@@ -111,6 +156,9 @@ bool kb_process_record(uint16_t keycode, bool key_pressed)
         case FACT_RESET:
             if (key_pressed && reset_mode_active) {
                 indicators_factory_reset();
+#ifdef RF_ENABLED
+                rf_factory_reset_bonds();
+#endif
             }
             return false;
         case RGB_FX_NEXT:
@@ -173,7 +221,39 @@ bool kb_process_record(uint16_t keycode, bool key_pressed)
         case LNK_BT3:
         case LNK_24G:
             if (user_keyboard_state.conn_mode == KEYBOARD_CONN_MODE_RF) {
-                rf_set_link(kb_keycode_to_rf_mode(keycode));
+                if (key_pressed) {
+                    rf_mode_t mode = kb_keycode_to_rf_mode(keycode);
+                    dprintf("rf link selected %02x\r\n", mode);
+                    rf_set_link(mode);
+                    user_settings.rf_link = (uint8_t)mode;
+                    settings_mark_dirty();
+                    keyboard_state.rf_link   = (uint8_t)mode;
+                    keyboard_state.connected = 1;
+                    keyboard_state.paired    = 1;
+                    link_hold_keycode  = keycode;
+                    link_hold_ticks    = 0;
+                    link_pairing_armed = true;
+                } else {
+                    if (link_hold_keycode == keycode) {
+                        link_hold_keycode  = 0;
+                        link_pairing_armed = false;
+                    }
+                }
+            }
+            return false;
+        case BAT_FLASH:
+            if (key_pressed) {
+                indicators_battery_flash();
+            }
+            return false;
+        case BAT_ON:
+            if (key_pressed) {
+                indicators_battery_on();
+            }
+            return false;
+        case BAT_OFF:
+            if (key_pressed) {
+                indicators_battery_off();
             }
             return false;
 #endif
@@ -224,18 +304,30 @@ void kb_send_extra(__xdata report_extra_t *report)
     }
 }
 
-uint16_t ticks = 0;
+__xdata uint16_t ticks = 0;
 
 void kb_update()
 {
 #ifdef RF_ENABLED
     if (user_keyboard_state.conn_mode == KEYBOARD_CONN_MODE_RF) {
-        if (ticks > 20000) {
-            EA = 0;
-            rf_update_keyboard_state(&keyboard_state);
-            ticks = 0;
-            EA    = 1;
+        if (link_pairing_armed && link_hold_keycode) {
+            if (link_hold_ticks < LINK_PAIRING_HOLD_TICKS) {
+                link_hold_ticks++;
+            } else {
+                rf_mode_t mode = kb_keycode_to_rf_mode(link_hold_keycode);
+                dprintf("rf link pairing %02x\r\n", mode);
+                keyboard_state.paired    = 0;
+                keyboard_state.connected = 0;
+                rf_set_link_pairing(mode, &keyboard_state);
+                link_pairing_armed = false;
+            }
         }
+
+        rf_link_supervisor(&keyboard_state);
+
+        rf_send_pending_flush();
+
+        rf_blanking_tick();
     }
 #endif
     ticks++;
